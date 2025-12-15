ee2ad3bf1947b0bd215b1c612c7a88944d6322ee
ABisman1 <asherb40@nycstudents.net>
2025-12-15 14:55:39 -0500

    Update unit1PT.py

--- unit1PT.py
+++ unit1PT.py
@@ -7,28 +7,31 @@ editer = Tk()
 editer.title("Enter Title Here")
 
 #Set size of window
-editer.geometry("300x150")
+editer.geometry("500x500")
 
 #Creates label
-label = Label(editer, text='Enter Text Here')
+label = Label(editer, text='Enter Text Here', anchor=N,font=("Arial", 20, "bold"))
 
 # Create buttons
-save_button = Button(editer, text="Save")
+save_button = Button(editer, text="Save",font=("Arial", 20, "bold"))
 
 # Creates textbox
 text_box = Text(editer,)
 
-# Specify Grid
-Grid.rowconfigure(editer,0,weight=1)
-Grid.columnconfigure(editer,0,weight=1)
+# # Specify Grid
+# # Grid.rowconfigure(editer,0,weight=1,)
+# # Grid.columnconfigure(editer,0,weight=1)
 
-Grid.rowconfigure(editer,1,weight=1)
+# # Grid.rowconfigure(editer,1,weight=1)
 
-# Place widgets in window (with pack function!)
+# # Place widgets in window (with pack function!)
 
-save_button.grid(row=2,column=0,sticky='SW')
-text_box.grid(row=1,column=0, columnspan=3,sticky='NSEW')
-label.grid(row=0,column=1,sticky='N')
+sizeY=editer.winfo_width()
+
+label.pack(fill='both',expand=True)
+text_box.pack(fill='both',expand=True)
+save_button.pack(fill='both',expand=True,padx=(0,sizeY*4))
+
 
 # Start the GUI event loop
 editer.mainloop()
