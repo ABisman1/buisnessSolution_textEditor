d4ada5cc8c4970aee04bf867c7fd13fbee7cb35f
ABisman1 <asherb40@nycstudents.net>
2025-12-22 14:49:58 -0500

    For some reason I need to do this

---
+++ unit1PTcTK.py
@@ -0,0 +1,30 @@
+from tkinter import *
+#need to install on all machines
+from tkmacosx import Button
+
+# Create the main window
+editer = Tk()
+editer.title("Enter Title Here")
+
+#Set size of window
+editer.geometry("2000x1000")
+
+#Creates label
+label = Label(editer, text='Enter Text Here',font=("Arial", 20, "bold"))
+
+# Create buttons
+save_button = Button(editer, text="Save",font=("Arial", 20, "bold"))
+
+# Creates textbox
+text_box = Text(editer,width=200,height=55)
+
+sizeH=editer.winfo_height()
+sizeW=editer.winfo_width()
+
+label.grid(row=0,column=1,sticky='w',padx=325,pady=10)
+text_box.grid(row=1,column=0,columnspan=3,padx=30)
+save_button.grid(row=2,column=0,sticky='sw',padx=30)
+
+
+# Start the GUI event loop
+editer.mainloop()
