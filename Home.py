f6969f10ff2651ac89927b5ab3e42fc2ee253d1a
ABisman1 <asherb40@nycstudents.net>
2026-01-05 14:47:52 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,6 +2,12 @@ from tkinter import *
 #need to install on all machines
 from tkmacosx import Button
 
+def new():
+	pass
+
+def view():
+	pass
+
 # Create the main window
 root = Tk()
 root.title("Home")
@@ -10,18 +16,18 @@ root.title("Home")
 root.geometry("2000x1000")
 
 # Create buttons
-new_button = Button(root, text="Open New File", font=("Arial", 20, "bold"))
-view_button = Button(root, text="View Files",font=("Arial", 20, "bold"))
+new_button = Button(root, text="Open New File", font=("Arial", 20, "bold"),height=40,width=175,command=new)
+view_button = Button(root, text="View Files",font=("Arial", 20, "bold"),height=40,width=175,command=view)
 
 #Add a label
-label = Label(root, text="Welcome to Note Taker")
-label_1 = Label(root, text="Select to Continue")
+label = Label(root, text="Welcome to Note Taker",font=("Arial", 20, "bold"))
+label_1 = Label(root, text="Select to Continue",font=("Arial", 20, "bold"))
 
 # Place widgets in window (with pack function!)
-new_button.grid(row=2,column=0)
-label.grid(row=0,column=1)
-view_button.grid(row=2,column=2,sticky='s',pady=100)
-label_1.grid(row=1,column=1)
+new_button.grid(row=2,column=0,padx=200)
+label.grid(row=0,column=1,padx=47,pady=50)
+view_button.grid(row=2,column=2,padx=200,pady=150)
+label_1.grid(row=1,column=1,pady=25)
 
 # Start the GUI event loop
 root.mainloop()
