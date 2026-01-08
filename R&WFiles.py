cf9d315e869938ab9f5d32dbf75f72712b0430ad
ABisman1 <asherb40@nycstudents.net>
2026-01-08 11:43:04 -0500

    jadsi f

---
+++ R&WFiles.py
@@ -0,0 +1,31 @@
+from tkinter import *
+#need to install on all machines
+from tkmacosx import Button
+
+def writeF():
+	text = text_box.get('1.0',END)
+	with open("demfile.txt", "w") as f:
+		f.write(text)
+		print("You entered: ")
+		print(text)
+
+def readF():
+	with open("demfile.txt", "r") as f:
+		content = f.read()
+		print(content)
+		text_box.insert('1.0', content)
+
+
+root = Tk()
+root.title("Reading & Writing Files")
+
+text_box = Text(root)
+
+write = Button(root, text='Write To File', command=writeF)
+readB = Button(root, text='Read From File', command=readF)
+
+text_box.pack()
+write.pack()
+readB.pack()
+
+root.mainloop()
