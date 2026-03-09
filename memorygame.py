3936010f815ced122a4fce202f1a7c29238e5e8b
ABisman1 <asherb40@nycstudents.net>
2026-03-09 14:41:57 -0400

    f

---
+++ memorygame.py
@@ -0,0 +1,17 @@
+from tkinter import *
+#need to install on all machines
+from tkmacosx import Button
+import os
+
+root = Tk()
+root.title("Home")
+
+#Set size of window
+root.geometry("2000x1000")
+
+
+Button1_1 = Button(root, )
+Button1_2 = Button(root, )
+Button2_1 = Button(root, )
+Button2_2 = Button(root, )
+
